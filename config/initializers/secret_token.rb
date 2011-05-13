045dd02c23f4a232bbfbe41f946bedd773a94c3b
Mat Schaffer <mat@schaffer.me>
2011-05-13 14:40:39 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimulchartRails::Application.config.secret_token = 'a6773b6343bec0d4f9bef75c647bfb2532c419905078f9bd291ac29104209c1921e2dec0f368e165987c323cf367840ebf353d402dfa56fa013d6c7b8932904e'
