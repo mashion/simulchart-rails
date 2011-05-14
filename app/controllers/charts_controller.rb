88076f5c141d820203d316b3aac9070c3b9f61e7
Trotter Cashion <cashion@gmail.com>
2011-05-14 12:04:08 -0400

    Update charts

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -2,11 +2,16 @@ class ChartsController < ApplicationController
   before_filter :find_chart, :except => :create
 
   def create
-    redirect_to Chart.create
+    redirect_to Chart.create(:points => [])
   end
 
   def show
     @charts = []
+  end
+
+  def update
+    @chart.update_attributes(params[:chart])
+    redirect_to @chart
   end
 
   def embed
