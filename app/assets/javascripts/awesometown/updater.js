977524bc3ac33b97893b2e42a42a6cd315ac8771
Trotter Cashion <cashion@gmail.com>
2011-05-14 19:07:09 -0400

    Update charts with Chloe

--- app/assets/javascripts/awesometown/updater.js
+++ app/assets/javascripts/awesometown/updater.js
@@ -1,10 +1,20 @@
 // id: ID on the server
 // graph: an Awesometown.Graph
-Awesometown.Updater = function (id, graph) {
-  var client = new Faye.Client('/faye', {timeout: 45}),
-      updates = '/' + id + '/updates';
- 
-  client.subscribe(updates, function (incoming) {
-    graph.add(incoming);
+Awesometown.Updater = function (id, graph, host, port) {
+  var client = new Chloe({host: host, port: port});
+
+  // TODO (trotter): Apparently Chloe won't receive any channel
+  //                 subscription messages if we don't have an
+  //                 onmessage defined.
+  client.onmessage(function () {
+    // Nothing, just a placeholder
+  });
+
+  client.connect(function () {
+    client.subscribe(id, function (incoming) {
+      console.log("Received data");
+      console.dir(incoming);
+      graph.add(JSON.parse(incoming));
+    });
   });
 };
