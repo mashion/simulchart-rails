d7f3df768d51b8ae1f98e64475923ee186f7b058
Trotter Cashion <cashion@gmail.com>
2011-05-15 22:44:53 -0400

    No longer need getAnchors

--- app/assets/javascripts/awesometown/graph.js
+++ app/assets/javascripts/awesometown/graph.js
@@ -139,27 +139,6 @@ Awesometown.Graph.prototype = {
     return this.memo.points;
   },
 
-  // Shamelessly yoinked from http://raphaeljs.com/analytics.html
-  getAnchors: function (p1x, p1y, p2x, p2y, p3x, p3y) {
-      var l1 = (p2x - p1x) / 2,
-          l2 = (p3x - p2x) / 2,
-          a = Math.atan((p2x - p1x) / Math.abs(p2y - p1y)),
-          b = Math.atan((p3x - p2x) / Math.abs(p2y - p3y));
-      a = p1y < p2y ? Math.PI - a : a;
-      b = p3y < p2y ? Math.PI - b : b;
-      var alpha = Math.PI / 2 - ((a + b) % (Math.PI * 2)) / 2,
-          dx1 = l1 * Math.sin(alpha + a),
-          dy1 = l1 * Math.cos(alpha + a),
-          dx2 = l2 * Math.sin(alpha + b),
-          dy2 = l2 * Math.cos(alpha + b);
-      return {
-          x1: p2x - dx1,
-          y1: p2y + dy1,
-          x2: p2x + dx2,
-          y2: p2y + dy2
-      };
-  },
-
   pointsToPath: function (points) {
     return "M," + points[0].x + "," + points[0].y + "L" + _.map(points.slice(1), function (point) {
               return point.x + "," + point.y;
