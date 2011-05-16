73765e075ea5336cbdf759b36a7c851568060069
Trotter Cashion <cashion@gmail.com>
2011-05-16 10:44:06 -0400

    Reload settings files in dev mode

--- config/initializers/settings.rb
+++ config/initializers/settings.rb
@@ -11,15 +11,23 @@ module Settings
 
   class Configs
     def initialize(dir)
+      @reload  = !Rails.application.config.cache_classes
       @configs = {}
       files = Dir[dir.join('*.yml')]
       files.each do |f|
         name = File.basename(f).sub(/.yml$/, '')
-        @configs[name] = YAML.load_file(f)[Rails.env]
+        load_config(name)
       end
     end
 
+    def load_config(name)
+      file = SETTINGS_DIR.join("#{name}.yml")
+      return unless file.exist?
+      @configs[name] = YAML.load_file(file)[Rails.env]
+    end
+
     def [](key)
+      load_config(key) if @reload
       @configs[key]
     end
   end
