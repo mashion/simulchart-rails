46e81b3f58aad4725cb24a6d35868af0a42581b8
Trotter Cashion <cashion@gmail.com>
2011-05-18 16:38:17 -0400

    Chloe bug was fixed

--- app/assets/javascripts/awesometown/updater.js
+++ app/assets/javascripts/awesometown/updater.js
@@ -3,13 +3,6 @@
 Awesometown.Updater = function (id, graph, host, port) {
   var client = new Chloe({host: host, port: port});
 
-  // TODO (trotter): Apparently Chloe won't receive any channel
-  //                 subscription messages if we don't have an
-  //                 onmessage defined.
-  client.onmessage(function () {
-    // Nothing, just a placeholder
-  });
-
   client.connect(function () {
     client.subscribe(id, function (incoming) {
       console.log("Received data");
