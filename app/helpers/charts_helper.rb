38a5324caabdcf9f58a2c258443556d02bceed4a
Trotter Cashion <cashion@gmail.com>
2011-05-29 11:51:13 -0400

    Lock down showing of update key

--- app/helpers/charts_helper.rb
+++ app/helpers/charts_helper.rb
@@ -2,4 +2,11 @@ module ChartsHelper
   def chart_embed(chart)
     content_tag :iframe, '', :src => embed_chart_url(chart, :width => 960, :height => 300), :frameBorder => 0
   end
+
+  # Very basic auth stuff. Will want to switch to cancan if
+  # we do anything else.
+  def owner?
+    return unless current_user
+    @chart.user_id == current_user
+  end
 end
