304862d0ec152cc4a9cfe43f012a070a8f259765
Trotter Cashion <cashion@gmail.com>
2011-06-13 15:43:32 -0400

    Try harder to get our db seed chart

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,11 @@
 class HomeController < ApplicationController
   def index
     @charts = current_user ? current_user.charts : []
-    @bieber = Chart.first
+    @bieber = Chart.order('created_at').first
   end
 
   def demos
     @load = Chart.first
-    @everyone = Chart.first
+    @everyone = Chart.order('created_at').first
   end
 end
